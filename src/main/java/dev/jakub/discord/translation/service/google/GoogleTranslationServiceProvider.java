03f744f60518bccca8e685e5314454456608e9ba
jakubbbdev <veritycsgo@gmail.com>
2025-08-02 23:04:38 +0200

    Add GoogleTranslationService and GoogleTranslationServiceProvider for Google Translate integration with support for emoji-based language options

---
+++ src/main/java/dev/jakub/discord/translation/service/google/GoogleTranslationServiceProvider.java
@@ -0,0 +1,8 @@
+package dev.jakub.discord.translation.service.google;
+
+public class GoogleTranslationServiceProvider extends GoogleTranslationService {
+
+    public GoogleTranslationServiceProvider(String authKey) {
+        super(authKey);
+    }
+}
