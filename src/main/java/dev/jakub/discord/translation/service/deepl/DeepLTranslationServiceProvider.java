733aa97858c2f1860f77385ffc3288856a4f0e32
jakubbbdev <veritycsgo@gmail.com>
2025-08-02 22:23:55 +0200

    Add DeepLTranslationServiceProvider class extending DeepLTranslationService

---
+++ src/main/java/dev/jakub/discord/translation/service/deepl/DeepLTranslationServiceProvider.java
@@ -0,0 +1,8 @@
+package dev.jakub.discord.translation.service.deepl;
+
+public class DeepLTranslationServiceProvider extends DeepLTranslationService {
+
+    public DeepLTranslationServiceProvider(String authKey) {
+        super(authKey);
+    }
+}
